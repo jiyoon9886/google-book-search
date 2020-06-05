a115779cd9a783e3522307c8aa2855f79e94eedd
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-04 21:19:47 -0400

    Created search route in app.js and modified Nav and Jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Books from "./pages/Books";
+import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 // The app will not render correctly until you setup a Route component.
 // Refer to the Basic Example documentation if you need to.
@@ -12,9 +14,13 @@ function App() {
   return (
     <Router>
       <Nav />
+			<Jumbotron>
+				<h1>(React) Google Book Search</h1>
+				<p>Search for and Save Books of Interest!</p>
+			</Jumbotron>
 			<Switch>
-			<Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
+			<Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
         <Route exact path="/books/:id" component={Detail} />
         <Route path="*" component={NoMatch} />
 			</Switch>
