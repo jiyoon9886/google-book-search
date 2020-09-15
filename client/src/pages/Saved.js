4906ca85acc7ab890217b64a23224c7c7bb71d1b
Jiyoon Kim <jiyoon9886@gmail.com>
2020-09-14 23:49:27 -0400

    added styling to 'No Results' div

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -29,71 +29,68 @@ const Saved = () => {
         }
     }
 
-    return(
-        <Container fluid>
-            <Jumbotron />
-            <Row>
-                {books.length ? (
-                    <div className="row row-cols-3" style={{ justifyContent:"center"}}>
-                        {books.map((book) => {
-                            return (
-                                <div
-                                    key={book._id}
-                                    className="card col-sm-3"
-                                    style={{ margin: "5px" }}
-                                >
-                                    <img
-                                        src={
-                                            book.image
-                                                ? book.image
-                                                : "http://icons.iconarchive.com/icons/paomedia/small-n-flat/128/book-icon.png"
-                                        }
-                                        className="card-img-top"
-                                        style={{ height: 300 }}
-                                        alt="..."
-                                    ></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title">
-                                            {book.title}
-                                        </h5>
-                                        <p className="card-text">
-                                            {book.description
-                                                ? book.description.length >=
-                                                    200
-                                                    ? book.description.slice(
-                                                            0,
-                                                            200
-                                                        )
-                                                    : book.description
-                                                : "No Description Available"}
-                                        </p>
-                                        <button
-                                            className="btn btn-primary"
-                                            onClick={() =>
-                                                handleDeleteBook(book._id)
-                                            }
-											>
-												Remove
-											</button>
-                                        <a
-                                            href={book.link}
-                                            rel="noopener noreferrer"
-                                            target={"_blank"}
-                                            className="card-link"
-                                        >
-                                            Book Link
-                                        </a>
-                                    </div>
-                                </div>
-                            );
-                        })}
+    return (
+      <Container fluid>
+        <Jumbotron />
+        <Row>
+          {books.length ? (
+            <div
+              className='row row-cols-3'
+              style={{ justifyContent: 'center' }}
+            >
+              {books.map((book) => {
+                return (
+                  <div
+                    key={book._id}
+                    className='card col-sm-3'
+                    style={{ margin: '5px' }}
+                  >
+                    <img
+                      src={
+                        book.image
+                          ? book.image
+                          : 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/128/book-icon.png'
+                      }
+                      className='card-img-top'
+                      style={{ height: 300 }}
+                      alt='...'
+                    ></img>
+                    <div className='card-body'>
+                      <h5 className='card-title'>{book.title}</h5>
+                      <p className='card-text'>
+                        {book.description
+                          ? book.description.length >= 200
+                            ? book.description.slice(0, 200)
+                            : book.description
+                          : 'No Description Available'}
+                      </p>
+                      <button
+                        className='btn btn-primary'
+                        onClick={() => handleDeleteBook(book._id)}
+                      >
+                        Remove
+                      </button>
+                      <a
+                        href={book.link}
+                        rel='noopener noreferrer'
+                        target={'_blank'}
+                        className='card-link'
+                      >
+                        Book Link
+                      </a>
                     </div>
-                ) : (
-                    <h3>No Results to Display</h3>
-                )}
-			</Row>
-        </Container>
-    )
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div style={{ padding: '20px', textAlign: 'center' }}>
+              <h3>No Results to Display</h3>
+            </div>
+          )}
+        </Row>
+      </Container>
+    );
 }
 
 export default Saved;
